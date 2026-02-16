27494e299ca26bf6f3b52f6c3ee73cfa6f46750e
KrishnanditoLksn <krislaksana123@gmail.com>
2026-02-16 13:30:25 +0700

    feat:add logger

--- app/db/database.py
+++ app/db/database.py
@@ -1,10 +1,18 @@
 import os
+import logging
 from dotenv import load_dotenv
 from sqlmodel import SQLModel, create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-load_dotenv()
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+logger = logging.getLogger(__name__)
+
+load_dotenv()
 
 DB_HOST = os.getenv("MYSQL_HOST")
 DB_PORT = os.getenv("MYSQL_PORT")
@@ -12,10 +20,34 @@ DB_NAME = os.getenv("MYSQL_DB")
 DB_USER = os.getenv("MYSQL_USER")
 DB_PASSWORD = os.getenv("MYSQL_PASSWORD")
 
+# Debug logging
+logger.info(f"DB_HOST: {DB_HOST}")
+logger.info(f"DB_PORT: {DB_PORT}")
+logger.info(f"DB_NAME: {DB_NAME}")
+logger.info(f"DB_USER: {DB_USER}")
+
+
+required_vars = {
+    "MYSQL_HOST": DB_HOST,
+    "MYSQL_PORT": DB_PORT,
+    "MYSQL_DB": DB_NAME,
+    "MYSQL_USER": DB_USER,
+    "MYSQL_PASSWORD": DB_PASSWORD,
+}
+
+
+try:
+    DB_PORT = int(DB_PORT) # type: ignore
+except ValueError:
+    logger.error("MYSQL_PORT must be an integer")
+    raise
+
 DATABASE_URL = (
     f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}"
-    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    f"@localhost:{DB_PORT}/{DB_NAME}"
 )
+
+logger.info(f"DATABASE_URL: {DATABASE_URL}")
 
 engine = create_engine(
     DATABASE_URL,
@@ -23,9 +55,9 @@ engine = create_engine(
 )
 
 SessionLocal = sessionmaker(
-    autoCommit = False,
+    autocommit=False,
     autoflush=False,
-    bind= engine
+    bind=engine
 )
 
 Base = declarative_base()
