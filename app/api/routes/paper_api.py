1f37abe03d48afa11a37d6749a0952aac5dab034
KrishnanditoLksn <krislaksana123@gmail.com>
2026-02-23 21:27:36 +0700

    feat:add recommendation endpoint for papers

--- app/api/routes/paper_api.py
+++ app/api/routes/paper_api.py
@@ -7,6 +7,7 @@ from app.db.database import get_db
 from app.repositories.paper_repository import PaperRepository
 from app.schemas.response.detail_paper_response import DetailPaperResponse, DetailPaperResponse
 from app.schemas.response.paper_response import PaperResponse
+from app.services import recommendation_service
 from app.services.paper_service import PaperService
 
 
@@ -72,3 +73,23 @@ def get_detail_paper(id,db:Session = Depends(get_db)):
                 }
         )
 
+@router.get("/recommend/{paper_id}")
+def get_recommendation(paper_id: int, top_n: int = 5 , db:Session=Depends(get_db)):
+
+    repo = PaperRepository(
+            db
+        )
+        
+    service = recommendation_service.RecommendationService(
+        repo
+    )
+    
+    results = service.get_recommendations_by_paper_id(paper_id,top_n)
+
+    if not results:
+        raise HTTPException(status_code=404, detail="Paper not found")
+
+    return {
+        "paper_id": paper_id,
+        "recommendations": results
+    }
