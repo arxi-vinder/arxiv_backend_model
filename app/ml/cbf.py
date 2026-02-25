e3ffbc179d9363ee2682adce187dc2b0b5c9cb0c
KrishnanditoLksn <krislaksana123@gmail.com>
2026-02-25 23:34:34 +0700

    delete:comment

--- app/ml/cbf.py
+++ app/ml/cbf.py
@@ -5,20 +5,11 @@ from sklearn.metrics.pairwise import cosine_similarity
 class ContentBasedFiltering:
     
     def __init__(self, datas, tfidf_vectorizer, tfidf_matrix):
-        """
-        datas: dataframe berisi title dan abstract
-        tfidf_vectorizer: model TF-IDF yang sudah di-fit
-        tfidf_matrix: hasil transform seluruh dokumen
-        """
         self.datas = datas
         self.vectorizer = tfidf_vectorizer
         self.tfidf_matrix = tfidf_matrix
     
-        
-    def get_recommendations_by_abstract(self, input_abstract, top_n=5):
-        """
-        Mengembalikan Top-N rekomendasi berdasarkan abstrak input
-        """
+    def get_recommendations_by_abstract(self, input_abstract, top_n):
 
         input_vector = self.vectorizer.transform([input_abstract])
         cosine_scores = cosine_similarity(input_vector, self.tfidf_matrix)
