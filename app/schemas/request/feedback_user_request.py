df7e829d6bd0e215d5504a32e5703bd566470346
KrishnanditoLksn <krislaksana123@gmail.com>
2026-05-11 22:58:18 +0700

    feat:optional atribute on feedback request

--- app/schemas/request/feedback_user_request.py
+++ app/schemas/request/feedback_user_request.py
@@ -1,6 +1,8 @@
 # schemas/feedback.py
 from pydantic import BaseModel
+from typing import Optional
 
 class FeedbackRequest(BaseModel):
     paper_id: int
-    response: int 
+    response: int
+    recommendations: Optional[list[int]] = None
