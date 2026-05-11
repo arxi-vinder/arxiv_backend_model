fa1840d1ce3633583148c6002097142bc9f53fa0
KrishnanditoLksn <krislaksana123@gmail.com>
2026-05-11 22:56:58 +0700

    refactor:move sql alchemy function into top

--- app/repositories/evaluation_repository.py
+++ app/repositories/evaluation_repository.py
@@ -1,7 +1,7 @@
 from sqlmodel import Session
 
 from app.model.evaluation_res import EvaluationResult
-
+from sqlalchemy import func
 
 class EvaluationRepository():
     
@@ -40,3 +40,37 @@ class EvaluationRepository():
     def delete_all(self):
         self.db.query(EvaluationResult).delete()
         self.db.commit()
+
+    def get_sum_precision(self):
+        """Get sum of all precision values"""
+        result = self.db.query(func.sum(EvaluationResult.precision)).scalar()
+        return result or 0
+
+    def get_sum_recall(self):
+        """Get sum of all recall values"""
+
+        result = self.db.query(func.sum(EvaluationResult.recall)).scalar()
+        return result or 0
+
+    def get_sum_f1_score(self):
+        """Get sum of all f1_score values"""
+        result = self.db.query(func.sum(EvaluationResult.f1_score)).scalar()
+        return result or 0
+
+    def get_sum_map(self):
+        """Get sum of all mean_average_precision (MAPE) values"""
+        result = self.db.query(func.sum(EvaluationResult.mean_average_precision)).scalar()
+        return result or 0
+
+    def get_metrics_by_k(self):
+        """Get sum and count of metrics grouped by k value"""
+        results = self.db.query(
+            EvaluationResult.k,
+            func.sum(EvaluationResult.precision).label('sum_precision'),
+            func.sum(EvaluationResult.recall).label('sum_recall'),
+            func.sum(EvaluationResult.f1_score).label('sum_f1_score'),
+            func.sum(EvaluationResult.mean_average_precision).label('sum_map'),
+            func.count(EvaluationResult.id).label('count')
+        ).group_by(EvaluationResult.k).all()
+
+        return results if results else []
